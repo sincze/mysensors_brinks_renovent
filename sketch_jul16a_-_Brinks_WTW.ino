4c576792e27c22d0ea2f3dbeeb65becf9c749347
sincze <sincze@gmail.com>
2017-07-18 21:41:29 +0200

    Add files via upload

---
+++ sketch_jul16a_-_Brinks_WTW.ino
@@ -0,0 +1,111 @@
+/**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * REVISION HISTORY
+ * Version 1.0 - Henrik Ekblad
+ *
+ * DESCRIPTION
+ * Example sketch showing how to control physical relays.
+ * This example will remember relay state after power failure.
+ * http://www.mysensors.org/build/relay
+ */
+
+// Enable debug prints to serial monitor
+#define MY_DEBUG
+
+// Enable and select radio type attached
+#define MY_RADIO_NRF24
+//#define MY_RADIO_RFM69
+
+// Enable repeater functionality for this node
+//#define MY_REPEATER_FEATURE
+
+// Flash leds on rx/tx/err
+
+// Set blinking period (in milliseconds)
+#define MY_DEFAULT_LED_BLINK_PERIOD 300
+
+//#define MY_DEFAULT_ERR_LED_PIN 4
+#define MY_DEFAULT_TX_LED_PIN 5               // I am using LED to display the activity TX on D5
+#define MY_DEFAULT_RX_LED_PIN 6             // I am using LED to display the activity RX on D6
+
+#define SKETCH_NAME        "Brinks WTW"
+#define SKETCH_VERSION     "1.0"
+
+
+#include <MySensors.h>
+
+#define RELAY_1  3  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
+#define NUMBER_OF_RELAYS 2 // Total number of attached relays
+#define RELAY_ON 1  // GPIO value to write to turn on attached relay
+#define RELAY_OFF 0 // GPIO value to write to turn off attached relay
+
+
+void before()
+{
+  for (int sensor=1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS; sensor++, pin++) {
+    // Then set relay pins in output mode
+    pinMode(pin, OUTPUT);
+    // Set relay to last known state (using eeprom storage)
+    digitalWrite(pin, loadState(sensor)?RELAY_ON:RELAY_OFF);
+  }
+}
+
+void setup()
+{
+
+}
+
+void presentation()
+{
+  Serial.println(F("-- Init MySensors Brinks WTW"));
+  // Send the sketch version information to the gateway and Controller
+  sendSketchInfo(SKETCH_NAME, SKETCH_VERSION);
+  Serial.print(F("NodeID: "));
+  Serial.println(getNodeId());
+
+  for (int sensor=1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS; sensor++, pin++) {
+    // Register all sensors to gw (they will be created as child devices)
+    present(sensor, S_BINARY);
+  }
+}
+
+
+void loop()
+{
+
+}
+
+void receive(const MyMessage &message)
+{
+  // We only expect one type of message from controller. But we better check anyway.
+  if (message.type==V_STATUS) {
+    // Change relay state
+    digitalWrite(message.sensor-1+RELAY_1, message.getBool()?RELAY_ON:RELAY_OFF);
+    // Store state in eeprom
+    saveState(message.sensor, message.getBool());
+    // Write some debug info
+    Serial.print("Incoming change for sensor:");
+    Serial.print(message.sensor);
+    Serial.print(", New status: ");
+    Serial.println(message.getBool());
+  }
+}
+
