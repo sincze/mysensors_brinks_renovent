e98d46b685186d28bc85b4c269f29b7eb9708761
sincze <sincze@gmail.com>
2017-11-19 13:45:03 +0100

    Added Heartbeat
    This unit is now supplied with a Heartbeat towards the Gateway.

--- sketch_jul16a_-_Brinks_WTW.ino
+++ sketch_jul16a_-_Brinks_WTW.ino
@@ -1,4 +1,9 @@
 /**
+ * Last Modified 27-09-2017 - S.Incze
+ * Reason: Due due NO / NC of Brinks installation
+ * Brinks Switches due to a link between 2 points. If Normally Closed is Used and Mysensors disconnects the WTW turns on.
+ * That is why Layout changed to connect brinks to NO pins.
+ * 
  * The MySensors Arduino library handles the wireless radio link and protocol
  * between your home built sensors/actuators and HA controller of choice.
  * The sensors forms a self healing radio network with optional repeaters. Each
@@ -28,14 +33,17 @@
  */
 
 // Enable debug prints to serial monitor
-#define MY_DEBUG
+#define MY_DEBUG                        // 27-09-2017 No Debug Data needed if not connected.
 
 // Enable and select radio type attached
 #define MY_RADIO_NRF24
 //#define MY_RADIO_RFM69
 
+// Assign a specific Node ID to this device
+#define MY_NODE_ID 25
+
 // Enable repeater functionality for this node
-//#define MY_REPEATER_FEATURE
+#define MY_REPEATER_FEATURE                 // 27-09-2017 Enabled repeater functionality
 
 // Flash leds on rx/tx/err
 
@@ -43,29 +51,30 @@
 #define MY_DEFAULT_LED_BLINK_PERIOD 300
 
 //#define MY_DEFAULT_ERR_LED_PIN 4
-#define MY_DEFAULT_TX_LED_PIN 5               // I am using LED to display the activity TX on D5
-#define MY_DEFAULT_RX_LED_PIN 6             // I am using LED to display the activity RX on D6
+#define MY_DEFAULT_TX_LED_PIN 6               // I am using LED to display the activity TX on D5
+#define MY_DEFAULT_RX_LED_PIN 7              // I am using LED to display the activity RX on D6
 
-#define SKETCH_NAME        "Brinks WTW"
-#define SKETCH_VERSION     "1.0"
+#define SKETCH_NAME        "Brinks WTW / OTGW"
+#define SKETCH_VERSION     "1.2"
 
 
 #include <MySensors.h>
 
 #define RELAY_1  3  // Arduino Digital I/O pin number for first relay (second on pin+1 etc)
-#define NUMBER_OF_RELAYS 2 // Total number of attached relays
-#define RELAY_ON 1  // GPIO value to write to turn on attached relay
-#define RELAY_OFF 0 // GPIO value to write to turn off attached relay
+#define NUMBER_OF_RELAYS 3 // Total number of attached relays
+#define RELAY_ON 0  // GPIO value to write to turn on attached relay 27-09-2017 Changed due to NO behaviour of the Brinks installation, Normally RELAY_ON 1
+#define RELAY_OFF 1 // GPIO value to write to turn off attached relay 27-09-2017 Changed due to NO behaviour of the Brinks installation RELAY_OFF 0
 
 
 void before()
-{
+{ 
   for (int sensor=1, pin=RELAY_1; sensor<=NUMBER_OF_RELAYS; sensor++, pin++) {
     // Then set relay pins in output mode
     pinMode(pin, OUTPUT);
     // Set relay to last known state (using eeprom storage)
     digitalWrite(pin, loadState(sensor)?RELAY_ON:RELAY_OFF);
   }
+  
 }
 
 void setup()
@@ -75,6 +84,7 @@ void setup()
 
 void presentation()
 {
+   
   Serial.println(F("-- Init MySensors Brinks WTW"));
   // Send the sketch version information to the gateway and Controller
   sendSketchInfo(SKETCH_NAME, SKETCH_VERSION);
@@ -88,9 +98,18 @@ void presentation()
 }
 
 
-void loop()
+void loop() // Added 5 minute heartbeat 07-11-2017
 {
+ const unsigned long fiveMinutes = 5 * 60 * 1000UL;
+ static unsigned long lastSampleTime = 0 - fiveMinutes;  // initialize such that a reading is due the first time through loop()
 
+ unsigned long now = millis();
+ if (now - lastSampleTime >= fiveMinutes)
+ {
+    lastSampleTime += fiveMinutes;
+    sendHeartbeat;
+ }
+ // add code to do other stuff here
 }
 
 void receive(const MyMessage &message)
